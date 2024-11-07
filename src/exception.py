6e020d67111e8bc5bb520ac21a8a5e5653658a19
Prem Poudel <prempoudelcsit079@chitwancollege.edu.np>
2024-11-07 15:18:22 +0545

    notebook added

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,10 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
+    """
+    error: Exception object
+    error_detail: object of sys module
+    """
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -11,6 +15,9 @@ def error_message_detail(error, error_detail:sys):
 
 
 class CustomException(Exception):
+    """
+    Custom Exception class for project
+    """
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
@@ -19,9 +26,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise CustomException(e, sys)
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
+#         raise CustomException(e, sys)
